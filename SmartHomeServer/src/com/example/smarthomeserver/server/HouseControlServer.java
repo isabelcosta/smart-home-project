8b752932ee2fd402d3c8d3e72e9681f43975028b
Isabel Costa <isabelcmdcosta@gmail.com>
2017-03-18 01:12:02 +0000

    added initial version of server

---
+++ SmartHomeServer/src/com/example/smarthomeserver/server/HouseControlServer.java
@@ -0,0 +1,18 @@
+package com.example.smarthomeserver.server;
+
+
+
+public class HouseControlServer {
+	public static int PORT = 9000;
+
+	public static void main(String[] args) {
+
+
+		DomoBusConfigLoader configLoader = new DomoBusConfigLoader("DomoBusSystemConfig.xml");
+
+
+		// start http server
+		SimpleHttpServer httpServer = new SimpleHttpServer();
+		httpServer.Start(PORT);
+	}
+}
