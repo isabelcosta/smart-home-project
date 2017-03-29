7ea87c327f4309fc13a6ab0e2d3d9c72ed6a1a63
Isabel Costa <isabelcmdcosta@gmail.com>
2017-03-29 01:34:14 +0100

    restructured code to have presentation package

--- SmartHomeApp/app/src/main/java/com/example/smarthomeapp/presentation/ui/SplashScreenActivity.java
+++ SmartHomeApp/app/src/main/java/com/example/smarthomeapp/presentation/ui/SplashScreenActivity.java
@@ -1,7 +1,9 @@
-package com.example.smarthomeapp;
+package com.example.smarthomeapp.presentation.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.smarthomeapp.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
