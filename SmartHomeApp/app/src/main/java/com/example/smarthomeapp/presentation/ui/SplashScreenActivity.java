84c373fd9770f3dd90bfc9a595c3b736805ba31b
Isabel Costa <isabelcmdcosta@gmail.com>
2017-03-30 17:05:32 +0100

    added template of login activity

--- SmartHomeApp/app/src/main/java/com/example/smarthomeapp/presentation/ui/SplashScreenActivity.java
+++ SmartHomeApp/app/src/main/java/com/example/smarthomeapp/presentation/ui/SplashScreenActivity.java
@@ -17,22 +17,22 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-//        /* New Handler to start the Menu-Activity
-//         * and close this Splash-Screen after some seconds.
-//         * */
-//        new Handler().postDelayed(new Runnable(){
-//            @Override
-//            public void run() {
-//                Intent mainIntent;
-//
-//                // TODO: 29/03/2017 create home/divisions activity
-//                mainIntent = new Intent(SplashScreenActivity.this, NextActivity.class);
-//
-//                /* Create an Intent that will start the Home Activity. */
-//                SplashScreenActivity.this.startActivity(mainIntent);
-//
-//                SplashScreenActivity.this.finish();
-//            }
-//        }, SPLASH_DISPLAY_LENGTH);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.
+         * */
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent;
+
+                // TODO: 29/03/2017 create home/divisions activity
+                mainIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+
+                /* Create an Intent that will start the Home Activity. */
+                SplashScreenActivity.this.startActivity(mainIntent);
+
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
