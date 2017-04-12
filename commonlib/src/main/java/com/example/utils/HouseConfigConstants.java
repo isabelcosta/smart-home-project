ab141f6c59c31ff002c315bc8fb28651de6915cf
Isabel Costa <isabelcmdcosta@gmail.com>
2017-04-12 01:27:51 +0100

    basic configuration init | updated HouseConfigEntity | #1 #2

---
+++ commonlib/src/main/java/com/example/utils/HouseConfigConstants.java
@@ -0,0 +1,129 @@
+package com.example.utils;
+
+/**
+ * Created by isabelcosta on 11-Apr-17.
+ *
+ * Config House XML tags and attributes
+ */
+
+public class HouseConfigConstants {
+
+    /**
+     * Elements Tag Names
+     */
+    public static final String DOMO_BUS_SYSTEM = "DomoBusSystem";
+
+    public static final String FAVORITES = "Favorites";
+    public static final String FAVORITE_LIST = "FavoriteList";
+    public static final String FAVORITE_DEVICE = "FavoriteDevice";
+    public static final String FAVORITE_DIVISION = "FavoriteDivision";
+
+    public static final String DEVICE_STATE = "DeviceState";
+    public static final String DEVICE_STATE_LIST = "DeviceStateList";
+
+    public static final String SCENARIO = "Scenario";
+    public static final String SCENARIO_LIST = "ScenarioList";
+
+    public static final String ACTION = "Action";
+    public static final String ACTION_LIST = "ActionList";
+
+    public static final String DEVICE = "Device";
+    public static final String DEVICE_LIST = "DeviceList";
+
+    public static final String DEVICE_SERVICE = "DeviceService";
+    public static final String DEVICE_SERVICE_LIST = "DeviceServiceList";
+
+    public static final String DIVSION = "Division";
+    public static final String DIVSION_LIST = "DivisionList";
+
+    public static final String FLOOR = "Floor";
+    public static final String FLOOR_LIST = "FloorList";
+
+    public static final String SERVICE = "Service";
+    public static final String SERVICE_LIST = "ServiceList";
+
+    public static final String USER = "User";
+    public static final String USER_LIST = "UserList";
+
+    public static final String HOUSE = "House";
+
+    public static final String ACCESS_LEVEL_LIST = "AccessLevelList";
+
+
+    /**
+     * Attributes
+     */
+    public static final String ID = "ID";
+    public static final String NAME = "Name";
+    public static final String PASSWORD = "Password";
+
+    public static final String TYPE = "Type";
+    public static final String VERSION = "Version";
+    public static final String DATE = "Date";
+
+    public static final String REF_DEVICE = "RefDevice";
+    public static final String REF_DIVISION = "RefDivision";
+    public static final String REF_USER = "RefUser";
+    public static final String REF_PROPERTY = "RefProperty";
+
+    public static final String VALUE = "Value";
+    public static final String INVALID_VALUE = "InvalidValue";
+
+    public static final String ACCESS_LEVEL = "AccessLevel";
+    public static final String LEVEL = "Level";
+
+    public static final String ADDRESS = "Address";
+    public static final String PHONE = "Phone";
+    public static final String REF_DEVICE_TYPE = "RefDeviceType";
+    public static final String USER_BLOCKED = "UserBlocked";
+
+    public static final String REF_FLOOR = "RefFloor";
+    public static final String HEIGHT_ORDER = "HeightOrder";
+
+    public static final String DEVICE_TYPE = "DeviceType";
+    public static final String DEVICE_TYPE_LIST = "DeviceTypeList";
+
+    public static final String PROPERTY = "Property";
+    public static final String PROPERTY_LIST = "PropertyList";
+
+    public static final String DEVICE_CLASS = "DeviceClass";
+    public static final String DEVICE_CLASS_LIST = "DeviceClassList";
+
+
+    /**
+     * Value Conversion
+     */
+    public static final String CONVERSION_FORMULA = "ConversionFormula";
+    public static final String CONVERSION_FORMULA_LIST = "ConversionFormulaList";
+
+    public static final String CONVERSION_OBJECT = "ConversionObject";
+    public static final String CONVERSION_OBJECT_LIST = "ConversionObjectList";
+
+    public static final String SYSTEM_TO_USER_OBJ = "SystemToUserObj";
+    public static final String USER_TO_SYSTEM_OBJ = "UserToSystemObj";
+    public static final String DECIMAL_PLACES = "DecimalPlaces";
+
+    /**
+     * Value Types
+     */
+    public static final String SCALAR_VALUE_TYPE_LIST = "ScalarValueTypeList";
+    public static final String SCALAR_VALUE_TYPE = "ScalarValueType";
+
+    public static final String ENUM_VALUE_TYPE_LIST = "EnumValueTypeList";
+    public static final String ENUM_VALUE_TYPE = "EnumValueType";
+    public static final String ENUMERATED = "Enumerated";
+
+    public static final String ARRAY_VALUE_TYPE_LIST = "ArrayValueTypeList";
+    public static final String ARRAY_VALUE_TYPE = "ArrayValueType";
+    public static final String VALUE_CONVERSION = "ValueConversion";
+
+    public static final String MAX_LEN = "MaxLen";
+    public static final String REF = "Ref";
+    public static final String NUM_BITS = "NumBits";
+
+    public static final String MAX_VALUE = "MaxValue";
+    public static final String MIN_VALUE = "MinValue";
+    public static final String UNITS = "Units";
+    public static final String STEP = "Step";
+
+}
--- commonlib/src/main/java/com/example/utils/domain/HomeConfigEntity.java
+++ commonlib/src/main/java/com/example/utils/domain/HomeConfigEntity.java
@@ -1,9 +1,39 @@
 package com.example.utils.domain;
 
+import java.util.List;
+
 /**
  * Created by isabelcosta on 28/03/2017.
  */
 public class HomeConfigEntity {
 
 
+    private static HomeConfigEntity _instance;
+
+    private List<Device> _deviceList;
+    private List<Service> _serviceList;
+    private House _house;
+
+    public static HomeConfigEntity getInstance(){
+        if (_instance == null){
+            return new HomeConfigEntity();
+        }
+        return _instance;
+    }
+
+    public List<Device> getDeviceList() {
+        return _deviceList;
+    }
+
+    public List<Service> getServiceList() {
+        return _serviceList;
+    }
+
+    private House getHouse() {
+        return _house;
+    }
+
+    public List<Floor> getFloorList() {
+        return getHouse().getFloorList();
+    }
 }
