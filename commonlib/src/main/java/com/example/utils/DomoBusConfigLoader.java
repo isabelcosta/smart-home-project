eec5d0500b01b5fce780dfa535d3fb169a768b3a
Isabel Costa <isabelcmdcosta@gmail.com>
2017-04-20 01:40:16 +0100

    read config file and init xml parser #1, #2

--- commonlib/src/main/java/com/example/utils/DomoBusConfigLoader.java
+++ commonlib/src/main/java/com/example/utils/DomoBusConfigLoader.java
@@ -1,27 +1,30 @@
 package com.example.utils;
 
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.IOException;
+import java.util.List;
 
-import com.example.utils.domain.DeviceClass;
+import com.example.utils.domain.HomeConfigEntity;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import static com.example.utils.HouseConfigConstants.DEVICE_CLASS;
+import static com.example.utils.HouseConfigConstants.DEVICE_CLASS_LIST;
+import static com.example.utils.HouseConfigConstants.ID;
+import static com.example.utils.HouseConfigConstants.NAME;
 
 /**
  * Created by isabelcosta on 18/03/2017.
  */
 public class DomoBusConfigLoader {
 
-    private static DomoBusConfigLoader ourInstance = new DomoBusConfigLoader();
-
-    public static DomoBusConfigLoader getInstance() {
-        return ourInstance;
-    }
+    private static DomoBusConfigLoader _ourInstance = new DomoBusConfigLoader();
+    private String _configFilesPackage = "./commonlib/src/main/java/com/example/utils/configs";
+    private HomeConfigEntity _homeConfig;
 
     public DomoBusConfigLoader(){}
 
@@ -29,28 +32,73 @@ public class DomoBusConfigLoader {
 
     public DomoBusConfigLoader(String configFileName){
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setValidating(true);
-        factory.setIgnoringElementContentWhitespace(true);
         try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            File file = new File(configFileName);
-            Document doc = builder.parse(file);
 
-            // Do something with the document here.
-            createStructures(doc);
+            File directoryFiles = new File(_configFilesPackage); // your folder path
+            File[] filesList = directoryFiles.listFiles(); // It gives list of all files in the folder.
 
-        } catch (ParserConfigurationException e) {
-        } catch (SAXException e) {
-        } catch (IOException e) {
-            System.out.println("There is no file with the name:" + configFileName);
+            File file = null;
+
+            if (filesList != null) {
+                for (File f: filesList) {
+                    if (configFileName.equalsIgnoreCase(f.getName())) {
+                        file = f;
+                    }
+                }
+                if (file != null){
+                    SAXReader reader = new SAXReader();
+                    Document document = reader.read(file);
+
+                    // Do something with the document here.
+                    createStructures(document);
+                } else {
+                    throw new DocumentException();
+                }
+            } else {
+                throw new DocumentException();
+            }
         }
+        catch (DocumentException e) {
+            System.out.println("There is no file with the name: " + configFileName);
+            e.printStackTrace();
+        }
+    }
+
+    public static DomoBusConfigLoader getInstance() {
+        return _ourInstance;
     }
 
     private void createStructures(Document document){
 
         // Parse xml into structures
-        DeviceClass c;
+        HomeConfigEntity homeConfigInConstruction = new HomeConfigEntity();
+
+        System.out.println("Root element :" + document.getRootElement().getName());
+
+        // Root class is DomoBusSystem
+        Element classElement = document.getRootElement();
+
+        List<Node> nodes = classElement.selectNodes(DEVICE_CLASS_LIST + "/" + DEVICE_CLASS);
+        System.out.println("----------------------------\n");
+        for (Node node : nodes) {
+            System.out.println("Current Element :" + node.getName());
+            System.out.println("ID : " + node.valueOf(getAttribute(ID)));
+            System.out.println("Name : " + node.valueOf(getAttribute(NAME)));
+        }
+
+        _homeConfig = homeConfigInConstruction;
     }
 
+    public HomeConfigEntity getHomeConfig(){
+        return _homeConfig;
+    }
+
+    private String getAttribute(String attr){
+        return "@" + attr;
+    }
+    public static void main(String[] args) {
+
+        DomoBusConfigLoader configLoader = new DomoBusConfigLoader("basic-config-1.xml");
+
+    }
 }
