7c5f51086479f61df0532f540860088a100170f6
Isabel Costa <isabelcmdcosta@gmail.com>
2017-05-05 03:12:57 +0100

    icons here

--- app/src/main/java/com/example/smarthomeapp/util/Constants.java
+++ app/src/main/java/com/example/smarthomeapp/util/Constants.java
@@ -5,4 +5,14 @@ package com.example.smarthomeapp.util;
  */
 
 public class Constants {
+
+    interface DivisionType {
+        public String BEDROOM = "1";
+        public String KITCHEN = "2";
+        public String BATHROOM = "3";
+        public String HALL = "4";
+        public String ATTIC = "5";
+        public String LIVING_ROOM = "6";
+        public String GARDEN = "7";
+    }
 }
--- app/src/main/java/com/example/smarthomeapp/util/IconUtils.java
+++ app/src/main/java/com/example/smarthomeapp/util/IconUtils.java
@@ -1,8 +1,41 @@
 package com.example.smarthomeapp.util;
 
+import android.util.SparseIntArray;
+
+import com.example.smarthomeapp.R;
+
+import java.util.HashMap;
+
+import static com.example.smarthomeapp.util.Constants.DivisionType.ATTIC;
+import static com.example.smarthomeapp.util.Constants.DivisionType.BATHROOM;
+import static com.example.smarthomeapp.util.Constants.DivisionType.BEDROOM;
+import static com.example.smarthomeapp.util.Constants.DivisionType.GARDEN;
+import static com.example.smarthomeapp.util.Constants.DivisionType.HALL;
+import static com.example.smarthomeapp.util.Constants.DivisionType.KITCHEN;
+import static com.example.smarthomeapp.util.Constants.DivisionType.LIVING_ROOM;
+
 /**
  * Created by isabelcosta on 05-May-17.
  */
 
 public class IconUtils {
+
+
+//    static HashMap<String, Integer> iconsMap = new HashMap<>();
+
+    static {
+        HashMap<String, Integer> iconsMap = new HashMap<>();
+        iconsMap.put(BEDROOM, R.drawable.ic_bed);
+        iconsMap.put(KITCHEN, R.drawable.ic_fridge);
+        iconsMap.put(BATHROOM, R.drawable.ic_toilet);
+        iconsMap.put(HALL, R.drawable.ic_bed);
+        iconsMap.put(ATTIC, R.drawable.ic_bed);
+        iconsMap.put(LIVING_ROOM, R.drawable.ic_sofa);
+        iconsMap.put(GARDEN, R.drawable.ic_bed);
+    }
+
+
+
+
+
 }
