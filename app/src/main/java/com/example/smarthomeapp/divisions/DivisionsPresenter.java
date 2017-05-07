a1d021ac65f87fec639ec7426c3ef82c886806f1
Isabel Costa <isabelcmdcosta@gmail.com>
2017-05-07 23:39:35 +0100

    omproved divisions presenter | overall improvements | added remember me

---
+++ app/src/main/java/com/example/smarthomeapp/divisions/DivisionsPresenter.java
@@ -0,0 +1,8 @@
+package com.example.smarthomeapp.divisions;
+
+/**
+ * Created by isabelcosta on 06-May-17.
+ */
+
+public class DivisionsPresenter {
+}
---
+++ app/src/main/java/com/example/smarthomeapp/util/SharedPreferencesUtils.java
@@ -0,0 +1,8 @@
+package com.example.smarthomeapp.util;
+
+/**
+ * Created by isabelcosta on 07-May-17.
+ */
+
+public class SharedPreferencesUtils {
+}
